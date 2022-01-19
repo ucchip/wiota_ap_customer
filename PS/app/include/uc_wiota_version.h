6ad14f9901ceca3629c651a77b726b9e50aa95a5
wangjingping-88 <jingping.wang@ucchip.com>
2022-01-19 16:38:05 +0800

    update version file

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v0.041"
-#define GIT_UPDATE_VERSION_TIME  "Wed Dec 15 09:25:28 2021"
+#define GIT_TAIL_TAG   "v0.05"
+#define GIT_UPDATE_VERSION_TIME  "Tue Jan 18 16:47:17 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
