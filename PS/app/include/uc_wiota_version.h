f6d4231f24c117c7d07ecfc1c725468442690e8a
jingping.wang <76737259+wangjingping-88@users.noreply.github.com>
2022-07-28 14:06:03 +0800

    update bin and lib

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -3,7 +3,7 @@
 #define _UC_WIOTA_VERSION_H__
 
 #define GIT_TAIL_TAG   "v0.13_ap8088"
-#define GIT_UPDATE_VERSION_TIME  "Fri Jun 17 11:12:22 2022"
+#define GIT_UPDATE_VERSION_TIME  "Thu Jul 28 12:10:33 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
