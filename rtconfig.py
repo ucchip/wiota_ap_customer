14621eaea043eb5fe55ee2a7cdde3ada1709db2c
jingping.wang <1182784514@qq.com>
2022-10-08 16:46:58 +0800

    remove version cmd

--- rtconfig.py
+++ rtconfig.py
@@ -63,7 +63,7 @@ if PLATFORM == 'gcc':
     #    CFLAGS += ' -O2'
 
     POST_ACTION = SIZE + ' $TARGET\n'
-    POST_ACTION += './tools/current_version.exe PS/app/include/uc_wiota_version.h' + '\n'
+    #POST_ACTION += './tools/current_version.exe PS/app/include/uc_wiota_version.h' + '\n'
     POST_ACTION += './tools/bin_bswap32_to_h.exe -i ./bin/ap8288.bin -o ./flat_ap8288.h' + '\n'
     #POST_ACTION += 'riscv32-unknown-elf-objdump -d rtthread.elf > rtthread.asm' + '\n'
     POST_ACTION += './tools/bintools.exe -f tools/uboot.bin  -u $TARGET ' + TARGET_NAME + '\n'
